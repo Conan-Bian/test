8c90088ba5d89369143b6fd147af596662b652f4
Bc <1750736771@qq.com>
2019-11-10 22:18:49 +0800

    提交lab06

---
+++ lab06/lab06_extra.py
@@ -0,0 +1,28 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    x = -1
+    n = len(snacks)
+    def helper():
+        nonlocal x
+        x = (x + 1) % n
+        return snacks[x]
+    return helper
